99b224ff089d3f95e4a7c2489116babb04d33ec6
iftekar2 <iftekar.hossain123@gmail.com>
2023-11-12 17:22:45 -0500

    Adding Burgerm menu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,95 +1,140 @@
-import styled from "styled-components"; 
+import React, { useState } from "react";
+import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
-function Navbar() {
+const Navbar = () => {
+  const [burgerMenuVisible, setBurgerMenuVisible] = useState(false);
+
+  const toggleBurgerMenu = () => {
+    setBurgerMenuVisible(!burgerMenuVisible);
+  };
+
   return (
-    <NevSection>
-        <WebsiteName>
+    <NavContainer>
+      <WebsiteTitle>
         <NavLink to="/">
-            <h1>The Verse</h1>
+          <p>The Verse</p>
         </NavLink>
-        </WebsiteName>
-        <NavItems>
-            <ul>
-                <li>
-                <NavLink to="/home">Home</NavLink>
-                </li>
-                <li>
-                <NavLink to="/about">About</NavLink>
-                </li>
-                <li>
-                <NavLink to="/contact">Contact</NavLink>
-                </li>
-                <li>
-                <NavLink to="/blog">Blog</NavLink>
-                </li>
-                <li>
-                <NavLink to="/game-experience">Games / Digital Experience</NavLink>
-                </li>
-                <li>
-                <NavLink to="/game">Try Game</NavLink>
-                </li>
-            </ul>
-        </NavItems>
-    </NevSection>
-  )
-}
+      </WebsiteTitle>
 
-const NevSection = styled.div`
-    display: flex; 
-    justify-content: space-between; 
-    align-items: center; 
-    padding-left: 2rem; 
-    padding-right: 2rem; 
-    height: 3.75rem; 
-    position: sticky;
-    top: 0;
-    z-index: 1;
-    background-color: #212529;
-    color: red; 
-`; 
-const WebsiteName = styled.div`
-    color: white; 
+      <BurgerMenu onClick={toggleBurgerMenu}>
+        <Span />
+        <Span />
+        <Span />
+      </BurgerMenu>
 
-    h1{
-        font-size: 1.5rem; 
-    }
+      <NavItems isVisible={burgerMenuVisible}>
+        <ul>
+          <li>
+            <NavLink to="/home">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to="/blog">Blog</NavLink>
+          </li>
+          <li>
+            <NavLink to="/digitalexperiences">Games / Digital Experiences</NavLink>
+          </li>
+          <li>
+            <NavLink to="/trygame">Try Games</NavLink>
+          </li>
+        </ul>
+      </NavItems>
+    </NavContainer>
+  );
+};
 
-    a{
-        text-decoration: none; 
-        color: white; 
-    }
-`; 
-
-const NavItems = styled.div`
-    display: flex; 
-    justify-content: center; 
-    align-items: center; 
-    padding-top: 0.5rem;
-
-    ul {
-        list-style-type: none; 
-        display: flex; 
-        justify-content: center; 
-        align-items: center; 
-        gap: 2rem; 
-        font-size: 1.3rem; 
-    }
-
-    li {
-        cursor: pointer;
-    }
-
-    a{
-        text-decoration: none; 
-        color: #D3D3D4; 
-    }
-
-    .active {
-        color: white; 
-        font-weight: 600; 
-    }
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 2rem;
+  padding-right: 3rem;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  background-color: #212529;
 `;
 
+const WebsiteTitle = styled.div`
+  font-size: 1.3rem;
 
-export default Navbar
+  a {
+    text-decoration: none;
+    color: white;
+  }
+`;
+
+const BurgerMenu = styled.div`
+  @media (max-width: 950px) {
+    gap: 0.5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
+`;
+
+const Span = styled.div`
+  @media (max-width: 950px) {
+    height: 3px;
+    width: 35px;
+    background: white;
+    border-radius: 10px;
+  }
+`;
+
+const NavItems = styled.div`
+  ul {
+    display: flex;
+    flex-direction: center;
+    justify-content: center;
+    align-items: center;
+    list-style-type: none;
+    gap: 2rem;
+  }
+
+  @media (max-width: 950px) {
+    ul {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        list-style-type: none;
+        gap: 2rem;
+    }
+  }
+
+  li{
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+    color: #d3d3d4;
+  }
+
+  .active {
+    color: white;
+    font-weight: 600;
+  }
+
+  @media (max-width: 950px) {
+    display: ${({ isVisible }) => (isVisible ? "flex" : "none")};
+    position: absolute;
+    top: 60px;
+    left: 0;
+    width: 100%;
+    background-color: #212529;
+    z-index: 1;
+    flex-direction: column;
+  }
+`;
+
+export default Navbar;
