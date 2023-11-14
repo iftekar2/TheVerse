1fda4e86ee053a4088970c86c381ad417fec2360
iftekar2 <iftekar.hossain123@gmail.com>
2023-11-14 12:24:46 -0500

    Adding style to Quote.jsx

--- src/Pages/Home/HomeComponents/Quote.jsx
+++ src/Pages/Home/HomeComponents/Quote.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import styled from "styled-components";
 
 function Quote() {
-  return <div>Quote</div>;
+  return (
+    <QuoteSection>
+      <div className="quote">
+        <p>
+          "Every Person Has A Gift To Give. The Verse makes it easier for each
+          person to find and use their passion/purpose/gift in a meaningful
+          way."
+        </p>
+        <h4>Chris D. / In The Verse Team</h4>
+      </div>
+    </QuoteSection>
+  );
 }
+
+const QuoteSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  margin-top: 10rem;
+
+  div {
+    width: 40rem;
+    text-align: center;
+
+    p {
+      font-size: 1.5rem;
+    }
+
+    h4 {
+      font-size: 1.2rem;
+    }
+  }
+`;
 
 export default Quote;
