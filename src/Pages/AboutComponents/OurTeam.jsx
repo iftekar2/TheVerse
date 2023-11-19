cbf5ee5d7c8518ca40cd57af806b6544e2bec669
iftekar2 <iftekar.hossain123@gmail.com>
2023-11-18 23:17:07 -0500

    Adding Mobile mode to OurTeam.jsx content

--- src/Pages/AboutComponents/OurTeam.jsx
+++ src/Pages/AboutComponents/OurTeam.jsx
@@ -18,6 +18,21 @@ function OurTeam() {
             (tech/health/media)
           </p>
         </div>
+
+        {/* <div className="ourTeamImage">
+          <div className="brandonHoward">
+            <img
+              src="https://media.licdn.com/dms/image/C5603AQHxiETt2zLLIw/profile-displayphoto-shrink_400_400/0/1592431777262?e=1706140800&v=beta&t=igawHYdLWWUhjP5QgA2tH0bs4k1mW86Ey1OT6__eu3U"
+              alt="Brandon Howard"
+            />
+
+            <div className="brandonHowardDescription">
+              <h1>Brandon Howard</h1>
+              <p>Founder, Co-Founder</p>
+              <p>Platform Director/Health & Wellbeing Innovator</p>
+            </div>
+          </div>
+        </div> */}
       </div>
     </OurTeamSection>
   );
@@ -44,8 +59,30 @@ const OurTeamSection = styled.div`
       p {
         width: 52rem;
         font-size: 1.3rem;
+        margin-bottom: 2rem;
       }
     }
+
+    @media (max-width: 950px) {
+        .ourTeamTitle {
+          width: 20rem;
+          h1 {
+            text-align: center;
+            margin-bottom: 1rem;
+          }
+        }
+  
+        .ourTeamDescription {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          p {
+            width: 20rem;
+            text-align: center;
+            font-size: 1.3rem;
+            margin-bottom: 1.5rem;
+          }
+        }
   }
 `;
 
