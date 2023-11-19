25b2bbca2e91b227fce2d81b5f73251d970d2057
iftekar2 <iftekar.hossain123@gmail.com>
2023-11-18 20:14:52 -0500

    Creating WhatVerseDoes.jsx page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import AboutComponents from "./AboutComponents/AboutComponents";
+import WhatVerseDoes from "./AboutComponents/WhatVerseDoes";
 
 function About() {
   return (
     <AboutPage>
       <AboutComponents />
+      <WhatVerseDoes />
     </AboutPage>
   );
 }
---
+++ src/Pages/AboutComponents/WhatVerseDoes.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+function WhatVerseDoes() {
+  return (
+    <WhatVerseDoesSection>
+      <div className="image">
+        <img
+          src="https://img.freepik.com/free-vector/yoga-outdoor-flat-style_24908-57718.jpg?size=626&ext=jpg&ga=GA1.1.2032587638.1700353838&semt=sph"
+          alt="People doing Yoga"
+        />
+      </div>
+    </WhatVerseDoesSection>
+  );
+}
+
+const WhatVerseDoesSection = styled.div``;
+
+export default WhatVerseDoes;
