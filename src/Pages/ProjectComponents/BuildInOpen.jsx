f89b669233997cc95f7e9345003255ebfde77d82
iftekar2 <iftekar.hossain123@gmail.com>
2023-12-05 21:30:14 -0500

    Adding title and adding styling to BuildInOpen.jsx component

--- src/Pages/ProjectComponents/BuildInOpen.jsx
+++ src/Pages/ProjectComponents/BuildInOpen.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import styled from "styled-components";
 
 function BuildInOpen() {
-  return <div>BuildInOpen</div>;
+  return (
+    <BuildInOpenSection>
+      <div className="buildInOpen">
+        <BuildInOpenTitle>
+          <h2>Building In the Open</h2>
+        </BuildInOpenTitle>
+      </div>
+    </BuildInOpenSection>
+  );
 }
+
+const BuildInOpenSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5rem;
+`;
+
+const BuildInOpenTitle = styled.div`
+  h2 {
+    color: white;
+  }
+`;
 
 export default BuildInOpen;
