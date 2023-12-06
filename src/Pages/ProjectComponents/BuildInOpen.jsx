034dfa7ff4fb82cfae4930ecbb2141f7c4847252
iftekar2 <iftekar.hossain123@gmail.com>
2023-12-05 21:57:01 -0500

    Creating ProjectOne and adding content and adding styling to the content

--- src/Pages/ProjectComponents/BuildInOpen.jsx
+++ src/Pages/ProjectComponents/BuildInOpen.jsx
@@ -7,6 +7,14 @@ function BuildInOpen() {
         <BuildInOpenTitle>
           <h2>Building In the Open</h2>
         </BuildInOpenTitle>
+
+        <BuildInOpenProjects>
+          <div className="projectOne">
+            <img className="projectOneImage" alt="COMING SOON" />
+            <h2>Title</h2>
+            <p>Description</p>
+          </div>
+        </BuildInOpenProjects>
       </div>
     </BuildInOpenSection>
   );
@@ -22,6 +30,44 @@ const BuildInOpenSection = styled.div`
 const BuildInOpenTitle = styled.div`
   h2 {
     color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const BuildInOpenProjects = styled.div`
+  margin-top: 2rem;
+
+  .projectOne {
+    width: 25rem;
+    height: 45rem;
+    background: white;
+
+    .projectOneImage {
+      height: 30rem;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: #d9d9d9;
+    }
+
+    h2 {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 1rem;
+      font-size: 2rem;
+    }
+
+    p {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 0.5rem;
+      font-size: 1.3rem;
+    }
   }
 `;
 
