83f1675814c29e292cf9076f7da06714585a9e4a
iftekar2 <iftekar.hossain123@gmail.com>
2023-12-05 21:17:37 -0500

    Adding Image to KarmaAbout.jsx component

--- src/Pages/ProjectComponents/KarmaAbout.jsx
+++ src/Pages/ProjectComponents/KarmaAbout.jsx
@@ -1,18 +1,53 @@
-import React from "react";
 import styled from "styled-components";
 
 function KarmaAbout() {
   return (
     <KarmaAboutSection>
-      <KarmaAboutTitle>
-        <h1>Karma The Six Realm Game: Gamifing prosociality. </h1>
-      </KarmaAboutTitle>
+      <div className="karmaAboutContent">
+        <KarmaAboutTitle>
+          <h2>Karma The Six Realm Game: Gamifing prosociality. </h2>
+        </KarmaAboutTitle>
+
+        <KarmaAboutDescription>
+          <p>
+            The 6 realms can be understood as forms of existence, or states of
+            mind, into which beings are born according to their karma. The
+            realms can be viewed as situations in life or even personality
+            types. Game play begins in the “Soppana: The Ladder”, where a simple
+            mini game calculates your proscial score. This score determines
+            which of the 6 realms a player is born into. In each realm, a player
+            explores agency/choice using prosocial game mechanics.
+          </p>
+
+          <img
+            src="https://img.freepik.com/premium-photo/religion-hinduism_930683-9456.jpg?size=626&ext=jpg&ga=GA1.1.2032587638.1700353838&semt=sph"
+            alt="Karma"
+          />
+        </KarmaAboutDescription>
+      </div>
     </KarmaAboutSection>
   );
 }
 
-const KarmaAboutSection = styled.div``;
+const KarmaAboutSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5rem;
+`;
 
-const KarmaAboutTitle = styled.div``;
+const KarmaAboutTitle = styled.div`
+  color: white;
+`;
+
+const KarmaAboutDescription = styled.div`
+  margin-top: 2rem;
+
+  p {
+    width: 38rem;
+    color: white;
+    font-size: 1.3rem;
+  }
+`;
 
 export default KarmaAbout;
